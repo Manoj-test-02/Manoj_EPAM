36f77e8bbf0c15423aca6bf1f9f84c6269d7ac4c
Manoj-test-02 <manomanojarun02@gmail.com>
2025-08-08 11:52:48 +0530

    Portfoloio Clean up

--- e2e/models/login.model.ts
+++
@@ -1,7 +0,0 @@
-export const LoginPageModel = {
-  username_input: '[data-test="username"]',
-  //changed to wrong attribute to trigger self-healing
-  password_input: '[data-test="password"]',  
-  login_button: '[data-test="login-button"]',
-  error_message: '[data-test="error"]'
-};
--- e2e/support/timeouts.ts
+++
@@ -1,4 +0,0 @@
-import { setDefaultTimeout } from '@cucumber/cucumber';
-
-// Set a 30-second timeout for all steps
-setDefaultTimeout(30 * 1000);
